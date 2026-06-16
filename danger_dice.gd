1ab515e063eeb45d0d05bc25cfd9a929f7ff238b
Peter Drake <drake@lclark.edu>
2026-06-16 11:33:57 -0700

    Buttons on play page work

--- danger_dice.gd
+++ danger_dice.gd
@@ -1,14 +1,17 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# Buttons on title page
 	$TitlePage/VBoxContainer/HBoxContainer/VBoxContainerLeft/PlayButton.pressed.connect(_on_play_button_pressed)
 	$TitlePage/VBoxContainer/HBoxContainer/VBoxContainerLeft/HowToPlayButton.pressed.connect(_on_how_to_play_button_pressed)
 	$TitlePage/VBoxContainer/HBoxContainer/VBoxContainerLeft/OptionsButton.pressed.connect(_on_options_button_pressed)
 	$TitlePage/VBoxContainer/HBoxContainer/VBoxContainerRight/AccessibilityButton.pressed.connect(_on_accessibility_button_pressed)
 	$TitlePage/VBoxContainer/HBoxContainer/VBoxContainerRight/CreditsButton.pressed.connect(_on_credits_button_pressed)
 	$TitlePage/VBoxContainer/HBoxContainer/VBoxContainerRight/QuitButton.pressed.connect(_on_quit_button_pressed)
+	# Buttons on play page
+	$Play/VBoxContainer/HowToPlayButton.pressed.connect(_on_how_to_play_button_pressed)
+	$Play/VBoxContainer/OptionsButton.pressed.connect(_on_options_button_pressed)
+	$Play/VBoxContainer/QuitToMenuButton.pressed.connect(_on_quit_to_menu_button_pressed)
 
 func _on_play_button_pressed():
 	$TitlePage.visible = false
@@ -16,10 +19,12 @@ func _on_play_button_pressed():
 
 func _on_how_to_play_button_pressed():
 	$TitlePage.visible = false
+	$Play.visible = false
 	$HowToPlay.visible = true
 
 func _on_options_button_pressed():
 	$TitlePage.visible = false
+	$Play.visible = false
 	$Options.visible = true
 
 func _on_accessibility_button_pressed():
@@ -32,3 +37,7 @@ func _on_credits_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+func _on_quit_to_menu_button_pressed():
+	$Play.visible = false
+	$TitlePage.visible = true
