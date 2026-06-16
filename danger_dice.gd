e467e0706e0c23945d177da5dbfc1d7512569a5b
Peter Drake <drake@lclark.edu>
2026-06-16 11:47:16 -0700

    Linking up more buttons

--- danger_dice.gd
+++ danger_dice.gd
@@ -1,17 +1,28 @@
 extends Control
 
+var _previous_page
+
 func _ready() -> void:
 	# Buttons on title page
 	$TitlePage/VBoxContainer/HBoxContainer/VBoxContainerLeft/PlayButton.pressed.connect(_on_play_button_pressed)
 	$TitlePage/VBoxContainer/HBoxContainer/VBoxContainerLeft/HowToPlayButton.pressed.connect(_on_how_to_play_button_pressed)
-	$TitlePage/VBoxContainer/HBoxContainer/VBoxContainerLeft/OptionsButton.pressed.connect(_on_options_button_pressed)
+	$TitlePage/VBoxContainer/HBoxContainer/VBoxContainerLeft/OptionsButton.pressed.connect(_on_options_button_pressed.bind($TitlePage))
 	$TitlePage/VBoxContainer/HBoxContainer/VBoxContainerRight/AccessibilityButton.pressed.connect(_on_accessibility_button_pressed)
 	$TitlePage/VBoxContainer/HBoxContainer/VBoxContainerRight/CreditsButton.pressed.connect(_on_credits_button_pressed)
 	$TitlePage/VBoxContainer/HBoxContainer/VBoxContainerRight/QuitButton.pressed.connect(_on_quit_button_pressed)
 	# Buttons on play page
 	$Play/VBoxContainer/HowToPlayButton.pressed.connect(_on_how_to_play_button_pressed)
-	$Play/VBoxContainer/OptionsButton.pressed.connect(_on_options_button_pressed)
+	$Play/VBoxContainer/OptionsButton.pressed.connect(_on_options_button_pressed.bind($Play))
 	$Play/VBoxContainer/QuitToMenuButton.pressed.connect(_on_quit_to_menu_button_pressed)
+	# Buttons on options page
+	$Options/HBoxContainer/VBoxContainerLeft/ReturnButton.pressed.connect(_on_options_return_button_pressed)
+	# Buttons on accessibility page
+	$Accessibility/VBoxContainer/MenuButton.pressed.connect(_on_return_to_menu_button_pressed)
+	# Buttons on credits page
+	$Credits/VBoxContainer/MenuButton.pressed.connect(_on_return_to_menu_button_pressed)
+	# Buttons on how to play page
+	
+
 
 func _on_play_button_pressed():
 	$TitlePage.visible = false
@@ -22,9 +33,9 @@ func _on_how_to_play_button_pressed():
 	$Play.visible = false
 	$HowToPlay.visible = true
 
-func _on_options_button_pressed():
-	$TitlePage.visible = false
-	$Play.visible = false
+func _on_options_button_pressed(previous_page):
+	previous_page.visible = false
+	_previous_page = previous_page
 	$Options.visible = true
 
 func _on_accessibility_button_pressed():
@@ -40,4 +51,13 @@ func _on_quit_button_pressed():
 
 func _on_quit_to_menu_button_pressed():
 	$Play.visible = false
+	$TitlePage.visible = true
+
+func _on_options_return_button_pressed():
+	$Options.visible = false
+	_previous_page.visible = true
+
+func _on_return_to_menu_button_pressed():
+	$Accessibility.visible = false
+	$Credits.visible = false
 	$TitlePage.visible = true
