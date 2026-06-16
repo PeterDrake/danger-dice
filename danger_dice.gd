0bacf73c99650969b9dce4add2c473c8690702a6
Peter Drake <drake@lclark.edu>
2026-06-16 13:41:21 -0700

    All buttons navigating between pages are set up

--- danger_dice.gd
+++ danger_dice.gd
@@ -5,13 +5,13 @@ var _previous_page
 func _ready() -> void:
 	# Buttons on title page
 	$TitlePage/VBoxContainer/HBoxContainer/VBoxContainerLeft/PlayButton.pressed.connect(_on_play_button_pressed)
-	$TitlePage/VBoxContainer/HBoxContainer/VBoxContainerLeft/HowToPlayButton.pressed.connect(_on_how_to_play_button_pressed)
+	$TitlePage/VBoxContainer/HBoxContainer/VBoxContainerLeft/HowToPlayButton.pressed.connect(_on_how_to_play_button_pressed.bind($TitlePage))
 	$TitlePage/VBoxContainer/HBoxContainer/VBoxContainerLeft/OptionsButton.pressed.connect(_on_options_button_pressed.bind($TitlePage))
 	$TitlePage/VBoxContainer/HBoxContainer/VBoxContainerRight/AccessibilityButton.pressed.connect(_on_accessibility_button_pressed)
 	$TitlePage/VBoxContainer/HBoxContainer/VBoxContainerRight/CreditsButton.pressed.connect(_on_credits_button_pressed)
 	$TitlePage/VBoxContainer/HBoxContainer/VBoxContainerRight/QuitButton.pressed.connect(_on_quit_button_pressed)
 	# Buttons on play page
-	$Play/VBoxContainer/HowToPlayButton.pressed.connect(_on_how_to_play_button_pressed)
+	$Play/VBoxContainer/HowToPlayButton.pressed.connect(_on_how_to_play_button_pressed.bind($Play))
 	$Play/VBoxContainer/OptionsButton.pressed.connect(_on_options_button_pressed.bind($Play))
 	$Play/VBoxContainer/QuitToMenuButton.pressed.connect(_on_quit_to_menu_button_pressed)
 	# Buttons on options page
@@ -21,19 +21,21 @@ func _ready() -> void:
 	# Buttons on credits page
 	$Credits/VBoxContainer/MenuButton.pressed.connect(_on_return_to_menu_button_pressed)
 	# Buttons on how to play page
-	
+	$HowToPlay/VBoxContainer/ReturnButton.pressed.connect(_on_how_to_play_return_button_pressed)
 
 
 func _on_play_button_pressed():
 	$TitlePage.visible = false
 	$Play.visible = true
 
-func _on_how_to_play_button_pressed():
-	$TitlePage.visible = false
-	$Play.visible = false
+func _on_how_to_play_button_pressed(previous_page):
+	# This button is on both the Title and Play pages
+	previous_page.visible = false
+	_previous_page = previous_page
 	$HowToPlay.visible = true
 
 func _on_options_button_pressed(previous_page):
+	# This button is on both the Title and Play pages
 	previous_page.visible = false
 	_previous_page = previous_page
 	$Options.visible = true
@@ -58,6 +60,11 @@ func _on_options_return_button_pressed():
 	_previous_page.visible = true
 
 func _on_return_to_menu_button_pressed():
+	# This button is on both the Accessibility and Credits pages
 	$Accessibility.visible = false
 	$Credits.visible = false
 	$TitlePage.visible = true
+
+func _on_how_to_play_return_button_pressed():
+	$HowToPlay.visible = false
+	_previous_page.visible = true
