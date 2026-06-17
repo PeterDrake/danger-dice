a0ba4d0950f5dd417969dcc523e30b1adb1f33a0
Saph <lc23-0425@lclark.edu>
2026-06-17 16:43:21 -0700

    Added focused textures to disabled, selected dice

--- die.gd
+++ die.gd
@@ -3,6 +3,8 @@ extends TextureButton
 var faces
 var pressed_faces
 var focused_faces
+var pressed_focused_faces
+var disabled_focused = load("res://PlaceholderAssets/die_disabled_focused.png")
 var current_face
 var rng = RandomNumberGenerator.new()
 
@@ -16,6 +18,9 @@ func _ready() -> void:
 	focused_faces = {1: load("res://PlaceholderAssets/die_focused_1.png"), 2: load("res://PlaceholderAssets/die_focused_2.png"),
 					 3: load("res://PlaceholderAssets/die_focused_3.png"), 4: load("res://PlaceholderAssets/die_focused_4.png"),
 					 5: load("res://PlaceholderAssets/die_focused_5.png"), 6: load("res://PlaceholderAssets/die_focused_6.png")}
+	pressed_focused_faces = {1: load("res://PlaceholderAssets/die_pressed_focused_1.png"), 2: load("res://PlaceholderAssets/die_pressed_focused_2.png"),
+							 3: load("res://PlaceholderAssets/die_pressed_focused_3.png"), 4: load("res://PlaceholderAssets/die_pressed_focused_4.png"),
+							 5: load("res://PlaceholderAssets/die_pressed_focused_5.png"), 6: load("res://PlaceholderAssets/die_pressed_focused_6.png")}
 	roll()
 	var image = texture_normal.get_image()
 	var bitmap = BitMap.new()
@@ -29,4 +34,13 @@ func roll():
 	texture_normal = faces[current_face]
 	texture_pressed = pressed_faces[current_face]
 	texture_focused = focused_faces[current_face]
+	texture_hover = texture_focused
+
+func _process(_delta: float) -> void:
+	if disabled:
+		texture_focused = disabled_focused
+	elif button_pressed:
+		texture_focused = pressed_focused_faces[current_face]
+	else:
+		texture_focused = focused_faces[current_face]
 	texture_hover = texture_focused
