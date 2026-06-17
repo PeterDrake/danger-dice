358d0f039d0b2b609d090823d446df99e973e076
Saph <lc23-0425@lclark.edu>
2026-06-17 09:25:19 -0700

    Added hexes, hex grid

---
+++ hex.gd
@@ -0,0 +1,7 @@
+extends TextureButton
+
+func _ready() -> void:
+	var image = texture_normal.get_image()
+	var bitmap = BitMap.new()
+	bitmap.create_from_image_alpha(image)
+	texture_click_mask = bitmap
