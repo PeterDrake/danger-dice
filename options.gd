319880d6cf5e31d7b9890e51f27e08412c224656
Peter Drake <drake@lclark.edu>
2026-06-17 09:46:03 -0700

    Must select exactly six dangers

--- options.gd
+++ options.gd
@@ -8,6 +8,8 @@ var default_dangers := []
 
 var dangers := []
 
+var _danger_count := 6
+
 func _ready() -> void:
 	_bus_index = AudioServer.get_bus_index("Master")
 	var slider = $HBoxContainer/VBoxContainerLeft/VBoxContainerVolume/VolumeSlider
@@ -35,6 +37,8 @@ func _ready() -> void:
 		$HBoxContainer/VBoxContainerRight/HBoxContainer/VBoxContainerRight/CheckBox8,
 		$HBoxContainer/VBoxContainerRight/HBoxContainer/VBoxContainerRight/CheckBox9
 	]
+	for checkbox in dangers:
+		checkbox.toggled.connect(_on_danger_checkbox_toggled)
 
 func _on_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(_bus_index, linear_to_db(value))
@@ -42,3 +46,11 @@ func _on_value_changed(value: float) -> void:
 func _on_default_danger_button_pressed() -> void:
 	for checkbox in dangers:
 		checkbox.button_pressed = checkbox in default_dangers
+	$HBoxContainer/VBoxContainerLeft/ReturnButton.disabled = false
+
+func _on_danger_checkbox_toggled(state: bool):
+	if state:
+		_danger_count += 1
+	else:
+		_danger_count -= 1
+	$HBoxContainer/VBoxContainerLeft/ReturnButton.disabled = (_danger_count != 6)
