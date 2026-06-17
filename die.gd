61f29124ef27e5a2e91b7e7797f5aa720d330165
Saph <lc23-0425@lclark.edu>
2026-06-17 10:42:05 -0700

    Added random faces to dice

--- die.gd
+++ die.gd
@@ -1,6 +1,12 @@
 extends TextureButton
 
+var faces
 func _ready() -> void:
+	faces = {1: load("res://PlaceholderAssets/die_1.png"), 2: load("res://PlaceholderAssets/die_2.png"),
+			 3: load("res://PlaceholderAssets/die_3.png"), 4: load("res://PlaceholderAssets/die_4.png"),
+			 5: load("res://PlaceholderAssets/die_5.png"), 6: load("res://PlaceholderAssets/die_5.png")}
+	var rng = RandomNumberGenerator.new()
+	texture_normal = faces[randi_range(1, 6)]
 	var image = texture_normal.get_image()
 	var bitmap = BitMap.new()
 	bitmap.create_from_image_alpha(image)
