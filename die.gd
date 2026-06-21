607f696ed082491a5435fe0ca40563e26e5c8db7
Saph <lc23-0425@lclark.edu>
2026-06-21 15:00:27 -0700

    Implemented official die assets

--- die.gd
+++ die.gd
@@ -4,23 +4,68 @@ var faces
 var pressed_faces
 var focused_faces
 var pressed_focused_faces
-var disabled_focused = load("res://PlaceholderAssets/die_disabled_focused.png")
+var disabled_focused = load("res://Assets/die_disabled_focused.png")
 var current_face
+var current_face_danger
 var rng = RandomNumberGenerator.new()
 
 func _ready() -> void:
-	faces = {1: load("res://PlaceholderAssets/die_1.png"), 2: load("res://PlaceholderAssets/die_2.png"),
-			 3: load("res://PlaceholderAssets/die_3.png"), 4: load("res://PlaceholderAssets/die_4.png"),
-			 5: load("res://PlaceholderAssets/die_5.png"), 6: load("res://PlaceholderAssets/die_6.png")}
-	pressed_faces = {1: load("res://PlaceholderAssets/die_pressed_1.png"), 2: load("res://PlaceholderAssets/die_pressed_2.png"),
-					 3: load("res://PlaceholderAssets/die_pressed_3.png"), 4: load("res://PlaceholderAssets/die_pressed_4.png"),
-					 5: load("res://PlaceholderAssets/die_pressed_5.png"), 6: load("res://PlaceholderAssets/die_pressed_6.png")}
-	focused_faces = {1: load("res://PlaceholderAssets/die_focused_1.png"), 2: load("res://PlaceholderAssets/die_focused_2.png"),
-					 3: load("res://PlaceholderAssets/die_focused_3.png"), 4: load("res://PlaceholderAssets/die_focused_4.png"),
-					 5: load("res://PlaceholderAssets/die_focused_5.png"), 6: load("res://PlaceholderAssets/die_focused_6.png")}
-	pressed_focused_faces = {1: load("res://PlaceholderAssets/die_pressed_focused_1.png"), 2: load("res://PlaceholderAssets/die_pressed_focused_2.png"),
-							 3: load("res://PlaceholderAssets/die_pressed_focused_3.png"), 4: load("res://PlaceholderAssets/die_pressed_focused_4.png"),
-							 5: load("res://PlaceholderAssets/die_pressed_focused_5.png"), 6: load("res://PlaceholderAssets/die_pressed_focused_6.png")}
+	faces = {
+			"Chainsaw":    load("res://Assets/die_normal/die_chainsaw.png"),
+			"Clown":       load("res://Assets/die_normal/die_clown.png"),
+			"Dentist":     load("res://Assets/die_normal/die_dentist.png"),
+			"Lava":        load("res://Assets/die_normal/die_lava.png"),
+			"Lightning":   load("res://Assets/die_normal/die_lightning.png"),
+			"Quicksand":   load("res://Assets/die_normal/die_quicksand.png"),
+			"Rattlesnake": load("res://Assets/die_normal/die_rattlesnake.png"),
+			"Shark":       load("res://Assets/die_normal/die_shark.png"),
+			"Spider":      load("res://Assets/die_normal/die_spider.png"),
+			"Vampire":     load("res://Assets/die_normal/die_vampire.png"),
+			"Wasp":        load("res://Assets/die_normal/die_wasp.png"),
+			"Wolf":        load("res://Assets/die_normal/die_wasp.png")
+	}
+	pressed_faces = {
+			"Chainsaw":    load("res://Assets/die_selected/die_selected_chainsaw.png"),
+			"Clown":       load("res://Assets/die_selected/die_selected_clown.png"),
+			"Dentist":     load("res://Assets/die_selected/die_selected_dentist.png"),
+			"Lava":        load("res://Assets/die_selected/die_selected_lava.png"),
+			"Lightning":   load("res://Assets/die_selected/die_selected_lightning.png"),
+			"Quicksand":   load("res://Assets/die_selected/die_selected_quicksand.png"),
+			"Rattlesnake": load("res://Assets/die_selected/die_selected_rattlesnake.png"),
+			"Shark":       load("res://Assets/die_selected/die_selected_shark.png"),
+			"Spider":      load("res://Assets/die_selected/die_selected_spider.png"),
+			"Vampire":     load("res://Assets/die_selected/die_selected_vampire.png"),
+			"Wasp":        load("res://Assets/die_selected/die_selected_wasp.png"),
+			"Wolf":        load("res://Assets/die_selected/die_selected_wolf.png")
+	}
+	focused_faces = {
+			"Chainsaw":    load("res://Assets/die_focused/die_focused_chainsaw.png"),
+			"Clown":       load("res://Assets/die_focused/die_focused_clown.png"),
+			"Dentist":     load("res://Assets/die_focused/die_focused_dentist.png"),
+			"Lava":        load("res://Assets/die_focused/die_focused_lava.png"),
+			"Lightning":   load("res://Assets/die_focused/die_focused_lightning.png"),
+			"Quicksand":   load("res://Assets/die_focused/die_focused_quicksand.png"),
+			"Rattlesnake": load("res://Assets/die_focused/die_focused_rattlesnake.png"),
+			"Shark":       load("res://Assets/die_focused/die_focused_shark.png"),
+			"Spider":      load("res://Assets/die_focused/die_focused_shark.png"),
+			"Vampire":     load("res://Assets/die_focused/die_focused_vampire.png"),
+			"Wasp":        load("res://Assets/die_focused/die_focused_wasp.png"),
+			"Wolf":        load("res://Assets/die_focused/die_focused_wasp.png")
+	}
+	pressed_focused_faces = {
+			"Chainsaw":    load("res://Assets/die_selected_focused/die_selected_focused_chainsaw.png"),
+			"Clown":       load("res://Assets/die_selected_focused/die_selected_focused_clown.png"),
+			"Dentist":     load("res://Assets/die_selected_focused/die_selected_focused_dentist.png"),
+			"Lava":        load("res://Assets/die_selected_focused/die_selected_focused_lava.png"),
+			"Lightning":   load("res://Assets/die_selected_focused/die_selected_focused_lightning.png"),
+			"Quicksand":   load("res://Assets/die_selected_focused/die_selected_focused_quicksand.png"),
+			"Rattlesnake": load("res://Assets/die_selected_focused/die_selected_focused_rattlesnake.png"),
+			"Shark":       load("res://Assets/die_selected_focused/die_selected_focused_shark.png"),
+			"Spider":      load("res://Assets/die_selected_focused/die_selected_focused_shark.png"),
+			"Vampire":     load("res://Assets/die_selected_focused/die_selected_focused_vampire.png"),
+			"Wasp":        load("res://Assets/die_selected_focused/die_selected_focused_wasp.png"),
+			"Wolf":        load("res://Assets/die_selected_focused/die_selected_focused_wasp.png")
+	}
 	roll()
 	var image = texture_normal.get_image()
 	var bitmap = BitMap.new()
@@ -28,11 +73,13 @@ func _ready() -> void:
 	texture_click_mask = bitmap
 
 func roll():
+	var danger_names = get_node("..").danger_names
 	disabled = false
 	current_face = rng.randi_range(1, 6)
-	texture_normal = faces[current_face]
-	texture_pressed = pressed_faces[current_face]
-	texture_focused = focused_faces[current_face]
+	current_face_danger = danger_names[current_face]
+	texture_normal = faces[current_face_danger]
+	texture_pressed = pressed_faces[current_face_danger]
+	texture_focused = focused_faces[current_face_danger]
 	texture_hover = texture_focused
 
 func _process(_delta: float) -> void:
@@ -40,9 +87,9 @@ func _process(_delta: float) -> void:
 		texture_focused = disabled_focused
 		accessibility_name = "Die already placed"
 	elif button_pressed:
-		texture_focused = pressed_focused_faces[current_face]
-		accessibility_name = "Selected die showing " + str(current_face)
+		texture_focused = pressed_focused_faces[current_face_danger]
+		accessibility_name = "Selected die showing " + str(current_face_danger)
 	else:
-		texture_focused = focused_faces[current_face]
-		accessibility_name = "Unselected die showing " + str(current_face)
+		texture_focused = focused_faces[current_face_danger]
+		accessibility_name = "Unselected die showing " + str(current_face_danger)
 	texture_hover = texture_focused
